43b4b02690c45d91cd3b61c36e2b6ee9e34ad2ea
Brandon-Price <brandon.samuel.price@gmail.com>
2023-08-31 14:39:59 -0500

    Updated the Back-end, TODO: Discount Code

--- backend/models/Discount.js
+++ backend/models/Discount.js
@@ -4,6 +4,7 @@ const DiscountSchema = new mongoose.Schema(
     {
         discount_code:{type: String, required: true, unique: true},
         price:{type: Number, required:true}
-    },{timestamps: true});
+    },
+    {timestamps: true});
 
 module.exports = mongoose.model("Discount", DiscountSchema);
