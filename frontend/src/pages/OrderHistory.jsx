655dc434c7d2e4d624844636c64789d83c67a2c4
Brandon-Price <brandon.samuel.price@gmail.com>
2023-09-22 14:33:36 -0500

    More clean up. Added more todos on some pages, added the order history page and route

---
+++ frontend/src/pages/OrderHistory.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import SubHeader from '../components/SubHeader';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
+const OrderHistory = () => {
+    return(
+        <div>
+            <Navbar/>
+            <SubHeader/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default OrderHistory;
