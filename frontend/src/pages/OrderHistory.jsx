5014ef6adf74e2202fdba0710ec06df58207a099
Brandon-Price <brandon.samuel.price@gmail.com>
2023-09-22 17:05:53 -0500

    Start of orders component and order history page

--- frontend/src/pages/OrderHistory.jsx
+++ frontend/src/pages/OrderHistory.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import SubHeader from '../components/SubHeader';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
+import Orders from '../components/Orders';
 
 const OrderHistory = () => {
     return(
         <div>
             <Navbar/>
             <SubHeader/>
+            <Orders/>
             <Footer/>
         </div>
     )
