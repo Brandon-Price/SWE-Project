7db4442d2fea2091a756e0ade3e24e16f0d2e9b3
Austin Moore <austin19moore@gmail.com>
2023-10-25 23:37:58 -0500

    Added api post on success

--- frontend/src/pages/Success.jsx
+++ frontend/src/pages/Success.jsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { useDispatch, useSelector } from "react-redux";
 import { toRemoveAll } from '../redux/cartRedux.js';
+import axios from "axios";
 
 const ButtonLink = styled(Link)`
     color: black;
@@ -17,8 +18,9 @@ const Success = () => {
     // dispatch is for the updating those values
     const dispatch = useDispatch();
     const user = useSelector((state) => state.user.currentUser);
-    const cartItems = useSelector(state => state.cart.quantity);
-
+    const cartItems = useSelector(state => state.cart);
+    console.log(user);
+    console.log(cartItems);
     // When you click on the button in the success page it removes all of the cart
     // So whent the user goes to the homepage nothing should be in the cart
     // You will also want to handle calling the api data to post the items to orders
@@ -26,8 +28,30 @@ const Success = () => {
       e.preventDefault();
       
       // temp, will implement removing items that were purchased from those available
-      // cartItems.forEach((item) => console.log(item));
+      // cartItems.forEach((item) => * remove from available *);
 
+      // post to api
+      axios.post("http://localhost:5000/api/orders/", {
+        userId: user._id,
+        products: cartItems.products,
+        // not sure why there is an amount required from the database when each product has an amount already, just gonna put 1
+        amount: 1,
+        // *** need to get this address
+        address: "test",
+        status: "Ordered"
+      }, {
+        headers: {
+        token: `Bearer ${user.accessToken}`
+      }
+    })
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error)
+      });
+
+      // remove from redux
       dispatch(toRemoveAll(cartItems))
   };
 
